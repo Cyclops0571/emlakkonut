1d1390e2c34fa0a4529bca24789011391211ac0a
arifunlu <arifunlu@gmail.com>
2017-07-12 18:30:40 +0300

    Parcels edit.

--- resources/views/parcels.blade.php
+++ resources/views/parcels.blade.php
@@ -7,7 +7,6 @@ input[type="file"] {
 .custom-file {
     margin-left: 1rem;
     margin-top: .5rem;
-    width: 100%;
 }
 .icon-size {
     font-size: 1.4rem;
@@ -25,8 +24,8 @@ input[type="file"] {
             <label class="custom-file">
                 <i class="icon-Quantity icon-size"></i>
                 <input type="file" id="inputParcel" class="form-control" aria-describedby="basic-addon1" onchange="fileUpload(this)">
-                <span id="spanParcel">Plan resmini yükleyiniz...</span>
             </label>
+            <input type="text" id="inputTParcel" class="form-control" placeholder="Parsel tipini giriniz..." aria-describedby="basic-addon1" autofocus>
             <button class="input-group-addon" id="basic-addon1"><i class="icon-Accept"></i></button>
             <button class="input-group-addon" id="basic-addon1"><i class="icon-Cancel"></i></button>
         </div>
