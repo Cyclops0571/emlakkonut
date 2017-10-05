656b5937c8cb7ef6b13ee7a213445570ed3ccdc5
Cyclops0571 <srdsaygili@gmail.com>
2017-10-06 01:53:52 +0300

    positions can be empty.

--- app/Http/Controllers/MapController.php
+++ app/Http/Controllers/MapController.php
@@ -8,22 +8,23 @@ use Illuminate\Http\Request;
 
 class MapController extends Controller
 {
-
     public function location()
     {
         $project = EstateProject::getCurrentProjectFromSession();
         $projectLocation = ProjectLocation::getByProjectID($project->id);
+
         return view('location', compact('projectLocation', 'project'));
     }
 
     public function save(Request $request)
     {
-        $this->validate($request, ['positions' =>'required', 'photo'  => 'mimes:jpeg,jpg|image']);
+        $this->validate($request, ['photo' => 'mimes:jpeg,jpg|image']);
+        $position = $request->get('positions', '[]');
         $projectId = EstateProject::getCurrentProjectIdFromSession();
         $projectLocation = ProjectLocation::getByProjectID($projectId);
-        $projectLocation->map_data = $request->get('positions', '');
+        $projectLocation->map_data = $position ?: '[]';
         $projectLocation->save();
-        if($request->file('photo')) {
+        if ($request->file('photo')) {
             $projectLocation->setPhoto($request->file('photo'));
         }
 
