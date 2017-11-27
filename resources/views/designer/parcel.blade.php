ff33e5e41b34c3d00a0970967fc0369edb0ffa06
arifunlu <arifunlu@gmail.com>
2017-11-28 02:16:43 +0300

    changes

--- resources/views/designer/parcel.blade.php
+++ resources/views/designer/parcel.blade.php
@@ -19,8 +19,8 @@
 @section('content')
     <h4 class="editor-title">Tasarlayıcı / Numarataj Planı</h4>
     <div id="wcp-editor"></div>
+    <div id="wcp-editor-list-title">Eklediklerim</div>
     <div class="wcp-eklediklerim">
-      <div id="wcp-editor-list-title">Eklediklerim</div>
       <div class="d-flex">
       <select>
         <option value="volvo">Volvo</option>
