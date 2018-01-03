1c082dad226e09ce7c2af5ee14c881dadd847180
Arif Ünlü <arifunlu@gmail.com>
2018-01-03 16:48:53 +0300

    changes

--- resources/views/numbering/create.blade.php
+++ resources/views/numbering/create.blade.php
@@ -2,7 +2,6 @@
 
 <style>
     .form-control {
-        margin: 0.375rem !important;
         padding: 0.375rem 0.75rem !important;
         border: 1px solid #ced4da !important;
         border-radius: 0.25rem !important;
@@ -16,30 +15,30 @@
         </div>
         <form name="numbering" method="post" action="{{URL::route('numbering.store')}}" class="form-group">
             {{csrf_field()}}
-            <div>
-                Numarataj İsmi
-                <input type="text" name="name" class="form-control">
+            <div class="form-group col-md-6">
+                <label for="idNumName">Numarataj İsmi</label>
+                <input id="idNumName" type="text" name="name" class="form-control">
             </div>
-            <div>
-                Ada
-                <select name="islandId" class="form-control">
+            <div class="form-group col-md-6">
+                <label for="idIsland">Ada</label>
+                <select id="idIsland" name="islandId" class="form-control">
                     <option>Hepsi</option>
                     @foreach($islands as $island)
                         <option value="{{$island->id}}">Ada: {{$island->island_kkys}}</option>
                     @endforeach
                 </select>
             </div>
-            <div>
-                Parcel
-                <select name="parcelId" class="form-control">
+            <div class="form-group col-md-6">
+                <label for="idParcel">Parcel</label>
+                <select id="idParcel" name="parcelId" class="form-control">
                     <option>Hepsi</option>
                     @foreach($parcels as $parcel)
                         <option value="{{$parcel->id}}">Parcel: {{$parcel->parcel}}</option>
                     @endforeach
                 </select>
             </div>
-            <div>
-                Blok
+            <div class="form-group col-md-6">
+                <label for="block">Blok</label>
                 <select name="blockId" id="block" class="form-control">
                     <option>Hepsi</option>
                     @foreach($blocks as $block)
@@ -47,8 +46,8 @@
                     @endforeach
                 </select>
             </div>
-            <div>
-                Kat
+            <div class="form-group col-md-6">
+                <label for="floorId">Kat</label>
                 <select name="floorId" id="floorId" class="form-control">
                     <option>Hepsi</option>
                 </select>
